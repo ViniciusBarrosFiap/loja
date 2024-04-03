0fa198485522bc6d6727f7097a06d3d4e5513832
ViniciusBarrosFiap <rm97824@fiap.com.br>
2024-04-03 12:14:52 -0300

    run lint

--- src/db/migrations/1711563100471-relaciona-pedido-e-itemPedido.ts
+++ src/db/migrations/1711563100471-relaciona-pedido-e-itemPedido.ts
@@ -1,53 +1,23 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class RelacionaPedidoEItemPedido1711563100471
+export class relacionaPedidoEItemPedido1685989050639
   implements MigrationInterface
 {
-  name = 'RelacionaPedidoEItemPedido1711563100471';
+  name = 'relacionaPedidoEItemPedido1685989050639';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "itens_pedido" DROP COLUMN "created_at"`,
+      `CREATE TABLE "itens_pedidos" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "quantidade" integer NOT NULL, "preco_venda" integer NOT NULL, "pedidoId" uuid, CONSTRAINT "PK_d93e780d333fe5d91e43797e8b5" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `ALTER TABLE "itens_pedido" DROP COLUMN "updated_at"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" DROP COLUMN "deleted_at"`,
-    );
-    await queryRunner.query(`ALTER TABLE "itens_pedido" ADD "pedidoId" uuid`);
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" DROP COLUMN "preco_venda"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" ADD "preco_venda" integer NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" ADD CONSTRAINT "FK_ab2b96858c45196d22cce672215" FOREIGN KEY ("pedidoId") REFERENCES "pedidos"("id") ON DELETE CASCADE ON UPDATE CASCADE`,
+      `ALTER TABLE "itens_pedidos" ADD CONSTRAINT "FK_aaa757efbf4f9fb222709a140b2" FOREIGN KEY ("pedidoId") REFERENCES "pedidos"("id") ON DELETE CASCADE ON UPDATE CASCADE`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "itens_pedido" DROP CONSTRAINT "FK_ab2b96858c45196d22cce672215"`,
+      `ALTER TABLE "itens_pedidos" DROP CONSTRAINT "FK_aaa757efbf4f9fb222709a140b2"`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" DROP COLUMN "preco_venda"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" ADD "preco_venda" character varying(255) NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" DROP COLUMN "pedidoId"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" ADD "deleted_at" TIMESTAMP`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" ADD "updated_at" TIMESTAMP NOT NULL DEFAULT now()`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "itens_pedido" ADD "created_at" TIMESTAMP NOT NULL DEFAULT now()`,
-    );
+    await queryRunner.query(`DROP TABLE "itens_pedidos"`);
   }
 }
