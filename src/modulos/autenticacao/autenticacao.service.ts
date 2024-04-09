4834c0f4815671dce67a74b14410bb24d3e7ff29
ViniciusBarrosFiap <rm97824@fiap.com.br>
2024-04-08 21:58:19 -0300

    criando verificação de credencial do usuario

--- src/modulos/autenticacao/autenticacao.service.ts
+++ src/modulos/autenticacao/autenticacao.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsuarioService } from '../usuario/usuario.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AutenticacaoService {
-  login(email: string, senha: string) {
-    console.log(email, senha);
+  constructor(private usuarioService: UsuarioService) {}
+
+  async login(email: string, senhaInserida: string) {
+    const usuario = await this.usuarioService.buscaPorEmail(email);
+
+    //.compare() faz a comparação da senha inserida com a senha no banco de dados
+    const usuarioFoiAutenticado = await bcrypt.compare(
+      senhaInserida,
+      usuario.senha,
+    );
+
+    if (!usuarioFoiAutenticado) {
+      throw new UnauthorizedException('email ou senha está incorreta');
+    }
+
+    console.log('Usuario autenticado');
   }
 }
--- src/modulos/usuario/usuario.module.ts
+++ src/modulos/usuario/usuario.module.ts
@@ -7,9 +7,9 @@ import { UsuarioEntity } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
 
 @Module({
-  // eslint-disable-next-line prettier/prettier
-  imports:[ TypeOrmModule.forFeature([UsuarioEntity])],
+  imports: [TypeOrmModule.forFeature([UsuarioEntity])],
   controllers: [UsuarioController],
   providers: [UsuarioService, UsuarioRepository, EmailEhUnicoValidator],
+  exports: [UsuarioService],
 })
 export class UsuarioModule {}
